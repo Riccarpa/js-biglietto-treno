db4e2442dd7e3770c7d4dec68de5d2a7bb4eea38
Riccarpa <riccardopagni99@gmail.com>
2021-07-01 16:13:14 +0200

    aggiunti sconti e totali in html

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ L'output del prezzo finale va stampato in forma umana (con massimo due decimali,
 var km = parseInt(prompt("numero di chilometri che vuole percorrere"));
 var age = parseInt(prompt("età del passeggero"));
 
-// age&number control
+// age&km control
 
 if (isNaN(km) || isNaN(age)) {
     alert('non hai inserito due numeri');
@@ -22,11 +22,11 @@ if (isNaN(km) || isNaN(age)) {
 var priceKm = "0.21";
 var fullPrice = km * priceKm;
 
-
-
 //discount
-var discount20 = fullPrice / 100 * 20;
-var discount40 = fullPrice / 100 * 40;
+var d20 = fullPrice / 100 * 20;
+var discount20 = d20.toFixed(2);
+var d40 = fullPrice / 100 * 40;
+var discount40 = d40.toFixed(2);
 
 //final prices
 var minors = fullPrice - discount20;
@@ -35,16 +35,34 @@ var finalPriceU18 = minors.toFixed(2);
 var eldest = fullPrice - discount40;
 var finalPriceO65 = eldest.toFixed(2);
 
-
 // ticket html link
 var ticket = document.getElementById("ticket");
+var discount = document.getElementById("discount");
+var tot = document.getElementById("tot");
 
 // age recognition
 if (age < 18) {
-    ticket.innerHTML = "Il prezzo è:" + " " + finalPriceU18 + "€";
+    ticket.innerHTML = "Il prezzo intero è:" + " " + fullPrice + " " +
+        "€";
+    discount.innerHTML = "Lo sconto è:" + " " + discount20 + " " +
+        "€";
+    tot.innerHTML = "Il prezzo finale è:" + " " + finalPriceU18 + " " +
+        "€";
 
 } else if (age >= 65) {
-    ticket.innerHTML = "Il prezzo è:" + " " + finalPriceO65 + "€";
+    ticket.innerHTML = "Il prezzo è:" + " " + finalPriceO65 + " " +
+        "€";
+    discount.innerHTML = "Lo sconto è:" + " " + discount40 + " " +
+        "€";
+    tot.innerHTML = "Il prezzo finale è:" + " " + finalPriceO65 + " " +
+        "€";
+
 } else {
-    ticket.innerHTML = "Il prezzo è:" + " " + fullPrice + "€";
+    ticket.innerHTML = "Il prezzo è:" + " " + fullPrice + " " +
+        "€";
+    discount.innerHTML = "Lo sconto è:" + " " + "0" + " " +
+        "€";
+    tot.innerHTML = "Il prezzo finale è:" + " " + fullPrice + " " +
+        "€";
+
 }
